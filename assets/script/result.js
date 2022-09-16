c24231f9fb7c8e63e0b800af3c5006cee60cda71
alcl2000 <amy.555.lewis@gmail.com>
2022-09-16 17:07:30 +0000

    javascript: created the results array for the results page

---
+++ assets/script/result.js
@@ -0,0 +1,14 @@
+const results =[
+    { i : 0,
+    title : "Regular Human Guy",
+    text : "You got several questions wrong. You might be due a rewatch of the show!"},
+    { i: 1,
+    title : "Novice Vampire Hunter",
+    text : "You got quite a few questions right! But you can never do too much research on vampires; maybe you should rewatch the show?" },
+    {i : 2,
+    title : "Familiar",
+    text : "You got an almost perfect score! You still haven't earned your unholy after life yet, but you have the rest of your life to try! "},
+    {i : 3,
+    title : "Vampiric Council Member",
+    text : "Perfect score! You'll be called before the council to organise the next party any time soon!"}
+]
