5553fd5e0931075efd756368ef7401e6898911e4
alcl2000 <amy.555.lewis@gmail.com>
2022-09-23 16:00:32 +0000

    script: assignResult if statement fully developed

--- assets/script/result.js
+++ assets/script/result.js
@@ -17,3 +17,29 @@ const results =[
 let resultTitle = document.getElementById('result-title');
 let resultPara = document.getElementById('result-text');
 let finalScore = document.getElementById('final-score');
+
+finalScore.innerText = localStorage.getItem('score');
+score = finalScore.innerHTML
+/**
+ * Checks the score the user and assigns them a value based on their score 
+ */
+function assignResult(score){
+    if(score == "0" || "1" || "2" || "3" || "4" ){
+        resultTitle.innerHTML = results[0].title;
+        resultPara.innerHTML = results[0].text;
+    }
+    else if (score == "5" || "6" || "7" ){
+        resultTitle.innerHTML = results[1].title;
+        resultPara.innerHTML = results[1].text;
+    }
+    else if (score == "8" || "9"){
+        resultTitle.innerHTML = results[2].title;
+        resultPara.innerHTML = results[2].text;   
+    }
+    else{
+        resultTitle.innerHTML = results[3].title;
+        resultPara.innerHTML = results[3].text;        
+    }
+
+}
+document.addEventListener('onLoad', assignResult(score));
