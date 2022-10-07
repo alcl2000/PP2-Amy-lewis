bd14b71db17e9bd2cab253405f52dec776108cdd
alcl2000 <amy.555.lewis@gmail.com>
2022-10-07 09:35:35 +0000

    script: array text edited

--- assets/script/result.js
+++ assets/script/result.js
@@ -1,16 +1,16 @@
 const results =[
     { i : 0,
     title : "Regular Human Guy",
-    text : "You got several questions wrong. You might be due a rewatch of the show!"},
+    text : "You got a lot of questions wrong. You better go and rewatch the show!"},
     { i: 1,
     title : "Novice Vampire Hunter",
     text : "You got quite a few questions right! But you can never do too much research on vampires; maybe you should rewatch the show?" },
     {i : 2,
     title : "Familiar",
-    text : "You got an almost perfect score! You still haven't earned your unholy after life yet, but you have the rest of your life to try! "},
+    text : "You got an almost perfect score! You still haven't earned your unholy after life yet, but I'm sure that your vampiric master will turn you any day now... "},
     {i : 3,
     title : "Vampiric Council Member",
-    text : "Perfect score! You'll be called before the council to organise the next party any time soon!"}
+    text : "Perfect score! You'll be called before the council to organise the next party very soon!"}
 ]
 
 //global
